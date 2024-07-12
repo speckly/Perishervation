6097ebe7df424fb4f03fe1a9b1933563ce77a1d5
Andrew Higgins <60218942+speckly@users.noreply.github.com>
2024-07-12 10:14:16 +0800

    add

--- constants.py
+++ constants.py
@@ -5,3 +5,10 @@ These constants may be modified due to changing needs.
 It is recommended that you understand the usage of the constant before making any changes"""
 
 THINKSPEAK_API_DELAY = 15 # Delay in seconds between dataset pushes to the Thingspeak channel
+TEMPHUMID_GPIO = 21 # DHT22 Temp and Humid
+LDR_GPIO = 9 # Light
+# Accelerometer
+ACCEL_SDA = 2
+ACCEL_SCL = 3 
+
+
